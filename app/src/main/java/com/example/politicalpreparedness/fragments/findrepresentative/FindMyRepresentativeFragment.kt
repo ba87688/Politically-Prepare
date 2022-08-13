e5ba1504f276df655e5505f93e86ae0c6b9a8e4e
Jagi <ba87688@gmail.com>
2022-08-13 05:12:28 -0500

    spinner and address retrieval

--- app/src/main/java/com/example/politicalpreparedness/fragments/findrepresentative/FindMyRepresentativeFragment.kt
+++ app/src/main/java/com/example/politicalpreparedness/fragments/findrepresentative/FindMyRepresentativeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.politicalpreparedness.fragments.findrepresentative
 
 import android.Manifest
+import android.app.Activity
 import android.content.pm.PackageManager
 import android.location.Address
 import android.location.Geocoder
@@ -10,6 +11,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
@@ -19,11 +22,13 @@ import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.material.snackbar.Snackbar
 import java.util.*
+import kotlin.collections.ArrayList
 
 val MY_PERMISSIONS_REQUEST_LOCATION = 0
 
 class FindMyRepresentativeFragment : Fragment() {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var otherList:ArrayList<String>
 
 
     lateinit var binding: FragmentFindMyRepresentativeBinding
@@ -34,12 +39,46 @@ class FindMyRepresentativeFragment : Fragment() {
         binding = FragmentFindMyRepresentativeBinding.inflate(inflater)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
 
+        otherList = ArrayList<String>(Arrays.asList(*resources.getStringArray(R.array.states)))
+
+        val spinner = binding.spinnerState
+
+        val c = ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.states,
+            android.R.layout.simple_spinner_item
+        )
+            .also { adapter ->
+
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spinner.adapter = adapter
+                spinner.onItemSelectedListener = object : AdapterView.OnItemClickListener,
+                    AdapterView.OnItemSelectedListener {
+                    override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                        TODO("Not yet implemented")
+                    }
+
+                    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                        Log.i("TAG", "onItemSelected: ind $position ")
+                    }
+
+                    override fun onNothingSelected(p0: AdapterView<*>?) {
+                        TODO("Not yet implemented")
+                    }
+
+                }
+            }
 
 
 
         binding.buttonFindMyRepresentative.setOnClickListener {
             val snackbar = Snackbar
-                .make(requireActivity(),requireView(), "You need to give permission to find your location", Snackbar.LENGTH_LONG)
+                .make(
+                    requireActivity(),
+                    requireView(),
+                    "You need to give permission to find your location",
+                    Snackbar.LENGTH_LONG
+                )
             snackbar.show()
         }
         binding.buttonUseMyLocation.setOnClickListener {
@@ -48,7 +87,7 @@ class FindMyRepresentativeFragment : Fragment() {
             val c = checkLocationPermission()
             Log.i("Messa", "onCreateView: ${c}")
 
-            if(c==true) {
+            if (c == true) {
                 fusedLocationClient.lastLocation
                     .addOnSuccessListener { location ->
                         Log.i("LOOL", "onCreateView:${location.longitude} ")
@@ -58,15 +97,16 @@ class FindMyRepresentativeFragment : Fragment() {
                             // use your location object
                             // get latitude , longitude and other info from this
                             val geocoder: Geocoder
-                            val addresses:List<Address>
-                            geocoder= Geocoder(requireContext(), Locale.getDefault())
-                            addresses = geocoder.getFromLocation(location.latitude,location.longitude,1)
+                            val addresses: List<Address>
+                            geocoder = Geocoder(requireContext(), Locale.getDefault())
+                            addresses =
+                                geocoder.getFromLocation(location.latitude, location.longitude, 1)
 
                             val address = addresses.get(0).getAddressLine(0)
                             val address0 = addresses.get(0).subThoroughfare
                             val address1 = addresses.get(0).thoroughfare
                             val city = addresses.get(0).locality
-                            val state = addresses.get(0).subAdminArea
+                            val state = addresses.get(0).adminArea
                             val country = addresses.get(0).countryName
                             val zipcode = addresses.get(0).postalCode
                             Log.i("ZIP CODE", "onCreateView: $zipcode ")
@@ -74,6 +114,18 @@ class FindMyRepresentativeFragment : Fragment() {
                             Log.i("ZIP CODE", "onCreateView: $address0 ")
                             Log.i("ZIP CODE", "onCreateView: $address1 ")
                             Log.i("ZIP CODE", "onCreateView: $city ")
+
+                            binding.etAddressLine1.setText(address0)
+                            binding.etAddressLine2.setText(address1)
+                            binding.etCity.setText(city)
+                            binding.spinnerState
+                            binding.etZipcode.setText(zipcode)
+
+                            val c = otherList.indexOf(state)
+                            Log.i("TAG", "onCreateView: state $state")
+                            Log.i("TAG", "onCreateView: state $c")
+                            binding.spinnerState.setSelection(c)
+
                         }
 
                     }
@@ -84,10 +136,12 @@ class FindMyRepresentativeFragment : Fragment() {
         return binding.root
     }
 
-    private fun checkLocationPermission():Boolean {
-        if (ActivityCompat.checkSelfPermission(requireContext(),
+    private fun checkLocationPermission(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
                 Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED) {
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
             // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(
                     requireActivity(),
@@ -118,6 +172,7 @@ class FindMyRepresentativeFragment : Fragment() {
             return true
         }
     }
+
     private fun requestLocationPermission() {
         ActivityCompat.requestPermissions(
             requireActivity(),
@@ -127,9 +182,6 @@ class FindMyRepresentativeFragment : Fragment() {
             MY_PERMISSIONS_REQUEST_LOCATION
         )
     }
-
-
-
 
 
 }
